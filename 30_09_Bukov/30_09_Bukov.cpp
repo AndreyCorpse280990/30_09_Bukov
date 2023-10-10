97eb2d4e366d87a5e0f8a22fc275797162dd210e
AndreyCorpse28090 <280990corpse@gmail.com>
2023-10-10 20:29:33 +0300

    1

--- 30_09_Bukov/30_09_Bukov.cpp
+++ 30_09_Bukov/30_09_Bukov.cpp
@@ -1,6 +1,34 @@
 ﻿#include <iostream>
 
+class People
+{
+private:
+    std::string name;
+    unsigned int age;
+
+public:
+// Конструктор по-умолчанию
+People() : name(0), age(0)
+{
+    std::cout << "Конструктор по умолчанию отработал" << std::endl;
+}
+
+// параметрический конструктор
+People(const std::string& nameP, int ageP) : name(nameP), age(ageP) {}
+{
+    std::cout << "Конструктор с параметрами отработал" << std::endl;
+}
+
+// Конструктор копирования
+People(const People& other) : name(other.name), age(other.age) {}
+
+// Деструктор
+~People()      
+{
+    std::cout << "Деструктор отработал" << std::endl;
+}
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
